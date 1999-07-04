f5516d4d789add7489c7d9d38f656bc206c660d4
steve <steve>
1999-07-04 23:55:47 +0000

    Initial revision

---
+++ Random.pm
@@ -0,0 +1,209 @@
+# String::Random - Generates a random string from a pattern
+# Copyright (C) 1998 Steven Pritchard <steve@silug.org>
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the same terms as Perl itself.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+package String::Random;
+
+use strict;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+use Exporter ();
+
+@ISA = qw(Exporter);
+@EXPORT_OK = qw(random_string);
+$VERSION = '0.1';
+
+use Carp;
+
+use vars qw(@upper @lower @digit @punct @any @salt %patterns);
+
+# These are the various character sets.
+@upper=("A".."Z");
+@lower=("a".."z");
+@digit=("0".."9");
+@punct=qw x~ ` ! @ $ % ^ & * ( ) - _ + = { } [ ] | \ : ; " ' . < > ? /x;
+push(@punct, "#", ","); # To avoid warnings when using -w
+@any=(@upper, @lower, @digit, @punct);
+@salt=(@upper, @lower, @digit, ".", "/");
+
+# What's important is how they relate to the pattern characters.
+# This could be done with anonymous array references,
+# but I tend to think that this is much more readable.
+%patterns = ( 'C' => \@upper,
+              'c' => \@lower,
+	      'n' => \@digit,
+	      '!' => \@punct,
+	      '.' => \@any,
+	      's' => \@salt );
+
+sub new
+{
+    my $proto=shift;
+    my $class=ref($proto) || $proto;
+    my $self;
+    %{$self}=%patterns; # makes $self a reference to a *copy* of %patterns
+    return bless($self, $class);
+}
+
+sub from_pattern
+{
+    my $self=shift;
+    croak "called without a reference" if (!ref($self));
+
+    my $string;
+
+LOOP:
+    my $pattern=shift;
+
+    foreach (split(//, $pattern))
+    {
+	if (defined($self->{$_}))
+	{
+	    $string.=$self->{$_}->[int(rand(scalar(@{$self->{$_}})))];
+	}
+	else
+	{
+	    croak qq(Unknown pattern character "$_"!);
+	}
+    }
+    goto LOOP if (@_); # Note that this was added as an afterthought, sorry.
+
+    return $string;
+}
+
+sub random_string
+{
+    my($pattern,@list)=@_;
+
+    my($n,%foo);
+
+    %foo=%patterns;
+
+    for ($n=0;$n<=$#list;$n++)
+    {
+	@{$foo{$n}}=@{$list[$n]};
+    }
+
+    return from_pattern(\%foo, $pattern);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+String::Random - Perl module to generate random strings based on a pattern
+
+=head1 SYNOPSIS
+
+  use String::Random;
+  $foo = new String::Random;
+  print $foo->from_pattern("..."); # Prints 3 random printable characters
+
+I<or>
+
+  use String::Random qw(random_string);
+  print random_string("..."); # Also prints 3 random characters
+
+=head1 DESCRIPTION
+
+This module makes it trivial to generate random strings.
+
+As an example, let's say you are writing a script that needs to generate a
+random password for a user.  The relevant code might look something like
+this:
+
+  use String::Random;
+  $pass = new String::Random;
+  print "Your password is ", $pass->from_pattern("CCcc!ccn"), "\n";
+
+This would output something like this:
+
+  Your password is UDwp$tj5
+
+=head2 Patterns
+
+The pre-defined patterns are as follows:
+
+  c	Any lowercase character [A-Z]
+  C	Any uppercase character [a-z]
+  n	Any digit [0-9]
+  !	A punctuation character [~`!@$%^&*()-_+={}[]|\:;"'.<>?/#,]
+  .	Any of the above
+  s	A "salt" character [A-Za-z0-9./]
+
+These can be modified, but if you need a different pattern it is better to
+create another pattern, possibly using one of the pre-defined as a base.
+For example, if you wanted a pattern C<A> that contained all upper and lower
+case letters (C<[A-Za-z]>), the following would work:
+
+  $foo = new String::Random;
+  $foo->{'A'} = [ 'A'..'Z', 'a'..'z' ];
+
+I<or>
+
+  $foo = new String::Random;
+  $foo->{'A'} = [ @{$foo->{'C'}}, @{$foo->{'c'}} ];
+
+The random_string function, described below, has an alternative interface
+for adding patterns.
+
+=head2 Methods
+
+=over 8
+
+=item from_pattern LIST
+
+The from_pattern method returns a random string based on the concatenation
+of all the pattern strings in the list.
+
+Please note that in a future revision, it will return a list of random
+strings corresponding to the pattern strings when used in list context.
+
+=back
+
+=head2 Functions
+
+=over 8
+
+=item random_string PATTERN,LIST
+
+=item random_string PATTERN
+
+When called with a single scalar argument, random_string returns a random
+string using that scalar as a pattern.  Optionally, references to lists
+containing other patterns can be passed to the function.  Those lists will
+be used for 0 through 9 in the pattern (meaning the maximum number of lists
+that can be passed is 10).  For example, the following code:
+
+  print random_string("0101",
+                      ["a", "b", "c"],
+                      ["d", "e", "f"]), "\n";
+
+would print something like this:
+
+  cebd
+
+=back
+
+=head1 BUGS
+
+As noted above, from_pattern doesn't do the right thing when called in a
+list context.  Whether it does the right thing in a scalar context when
+passed a list is up for debate.
+
+=head1 AUTHOR
+
+Steven Pritchard <steve@silug.org>
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
---
+++ test.pl
@@ -0,0 +1,111 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+######################### We start with some black magic to print on failure.
+
+# Change 1..1 below to 1..last_test_to_print .
+# (It may become useful if the test is moved to ./t subdirectory.)
+
+BEGIN { $| = 1; print "1..10\n"; }
+END {print "not ok 1\n" unless $loaded;}
+use String::Random;
+$loaded = 1;
+print "ok 1\n";
+
+######################### End of black magic.
+
+# Insert your test code below (better if it prints "ok 13"
+# (correspondingly "not ok 13") depending on the success of chunk 13
+# of the test code):
+
+$failed=0;
+
+# 2: Make sure we can create a new object
+$foo=new String::Random;
+$bar=new String::Random; # A spare for later
+if (!defined($foo) || !defined($bar))
+{
+    print "not ";
+    $failed++;
+}
+print "ok 2\n";
+
+# 3: Test function interface
+$abc=String::Random::random_string("012", ['a'], ['b'], ['c']);
+if ($abc ne 'abc')
+{
+    print "not ";
+    $failed++;
+}
+print "ok 3\n";
+
+# 4: Make sure the function didn't pollute $foo
+if (defined($foo->{'0'}))
+{
+    print "not ";
+    $failed++;
+}
+print "ok 4\n";
+
+# Try the object method...
+$foo->{'x'}=['a'];
+$foo->{'y'}=['b'];
+$foo->{'z'}=['c'];
+# 5: passing a scalar, in a scalar context
+$abc=$foo->from_pattern("xyz");
+if ($abc ne 'abc')
+{
+    print "not ";
+    $failed++;
+}
+print "ok 5\n";
+# 6: passing a list, in a scalar context
+$abc=$foo->from_pattern(qw(x y z));
+if ($abc ne 'abc')
+{
+    print "not ";
+    $failed++;
+}
+print "ok 6\n";
+
+# 7: Check one of the built-in patterns to make
+# sure it contains what we think it should
+@upcase=("A".."Z");
+for ($n=0;$n<26;$n++)
+{
+    if (!defined($foo->{'C'}->[$n]) || ($upcase[$n] ne $foo->{'C'}->[$n]))
+    {
+	print "not ";
+	$failed++;
+	last;
+    }
+}
+print "ok 7\n";
+
+# 8: Test modifying one of the built-in patterns
+$foo->{'C'}=['n'];
+if ($foo->from_pattern("C") ne "n")
+{
+    print "not ";
+    $failed++;
+}
+print "ok 8\n";
+
+# 9: Make sure we haven't clobbered anything in an existing object
+if ($bar->from_pattern("C") eq "n")
+{
+    print "not ";
+    $failed++;
+}
+print "ok 9\n";
+
+# 10: Make sure we haven't clobbered anything in a new object
+$baz=new String::Random;
+if (!defined($baz) || ($baz->from_pattern("C") eq "n"))
+{
+    print "not ";
+    $failed++;
+}
+print "ok 10\n";
+
+exit $failed;
