e1e1b046dbdf36c09b54a06d5753ba0f55d4cc42
Shlomi Fish <shlomif@shlomifish.org>
2013-12-03 12:39:28 +0200

    Add keywords and repository-link.

--- lib/String/Random.pm
+++ lib/String/Random.pm
@@ -27,7 +27,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 ) ] );
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = ();
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 # These are the various character sets.
 our @upper=("A".."Z");
