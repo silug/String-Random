99fc5441d264623b51de24a2c3f895c9586997df
Shlomi Fish <shlomif@shlomifish.org>
2014-01-31 17:20:59 +0200

    LICENSE file and section.

--- lib/String/Random.pm
+++ lib/String/Random.pm
@@ -446,6 +446,11 @@ Original Author: Steven Pritchard C<< steve@silug.org >>
 
 Now maintained by: Shlomi Fish ( L<http://www.shlomifish.org/> ).
 
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
 =head1 SEE ALSO
 
 perl(1).
